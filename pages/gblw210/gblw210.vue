f8c3d2b2ba406d3dc091a34f4f424e3b377b12a6
kuresaru <kuresaru@scraft.top>
2021-01-11 08:57:58 +0800

    增加gblw210内核下载地址

--- pages/gblw210/gblw210.vue
+++ pages/gblw210/gblw210.vue
@@ -5,7 +5,7 @@ div
     | 不知道该叫什么了 从隔壁老王收来的研究品 就直接叫它隔壁老王210了
   br
   br
-  
+
   p 当时感觉还挺好 后来又捡了一块 竟然便宜了qwq
   img(src="/v3_static/img/gblw210/tb-1.jpg")
   br
@@ -25,14 +25,23 @@ div
     li: a(href="https://github.com/kuresaru/uboot-gblw210", target="_blank") GitHub镜像(非及时更新)
 
   md-divider
-  h2 linux kernel 3.16.140 (usb,spi,nand无法使用)
-    p // TODO 待上传
+  h2 linux kernel 4.19.157 (主更新)
+  ul: h2 已测试可行功能:
+    li: | dm9000网卡
+    li: | NFS根文件系统
+    li: | SD卡
+  ul: h2 源码下载
+    li: a(href="https://git.scraft.top/kuresaru/linux-4.19.157-gblw210", target="_blank") 主仓库(限国内访问)
 
   md-divider
-  h2 linux kernel 5.6
-    p // TODO 待上传,未完成
-      br
-      | 没啥时间 肝不动了qaq
+  h2 linux kernel 3.16.85 (usb,spi,nand无法使用,已放弃更新)
+    p: a(href="https://git.scraft.top/kuresaru/kernel-3.16.85-gblw210", target="_blank") 主仓库(限国内访问)
+
+  //- md-divider
+  //- h2 linux kernel 5.6
+  //-   p // TODO 待上传,未完成
+  //-     br
+  //-     | 没啥时间 肝不动了qaq
 </template>
 
 <script lang="ts">
